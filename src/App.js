c07843e0e374f422be7bef5024da59596a603e35
Vahram <ramgrigoryan@gmail.com>
2022-06-24 10:33:46 +0400

    first part

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Component } from "react";
 import "./App.css";
+import CardList from "./components/CardList/CardList.component";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
+    console.log("constructor");
   }
 
   componentDidMount() {
@@ -26,16 +29,34 @@ class App extends Component {
           }
         );
       });
+    console.log("CDM");
   }
 
+  onSearchChange = (event) => {
+    let searchField = event.target.value.trim().toLowerCase();
+    this.setState(() => {
+      return {
+        searchField,
+      };
+    });
+  };
+
   render() {
+    console.log("render");
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredArray = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField)
+    );
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <div key={monster.id}>
-            <h1>{monster.name}</h1>
-          </div>
-        ))}
+        <input
+          className="search-input"
+          type="search"
+          placeholder="Monster name"
+          onChange={onSearchChange}
+        />
+       <CardList monsters={filteredArray}/>
       </div>
     );
   }
